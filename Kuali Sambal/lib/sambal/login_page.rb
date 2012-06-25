9cb73bc5121c1e37b61b5fb9fd5e163bec21e046
Abraham Heward <aheward@rsmart.com>
2012-06-25 07:39:49 -0700

    Added the login_with method. Changed the element definitions in the Login class.

--- Kuali Sambal/lib/sambal/login_page.rb
+++ Kuali Sambal/lib/sambal/login_page.rb
@@ -1,8 +1,15 @@
 class KualiStudent::Login < PageMaker
 
-  element(:username) { |b| b.text_field(:name=>"j_username") }
-  element(:password) { |b| b.text_field(:name=>"j_password") }
+  page_url "http://env1.ks.kuali.org/login.jsp" # TODO: This needs to be defined by a config variable, not hard-coded.
 
-  action(:login) { |b| b.button(:value=>"Login").click }
+  element(:username_field) { |b| b.text_field(:name=>"j_username") }
+  element(:password_field) { |b| b.text_field(:name=>"j_password") }
+  element(:login_button) { |b| b.button(:value=>"Login") }
+
+  def login_with username, password
+    username_field.set username
+    password_field.set password
+    login_button.click
+  end
 
 end
