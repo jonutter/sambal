00b14834ccef26c364da98f0f240e6a49e6287ee
Abraham Heward <aheward@rsmart.com>
2012-06-25 07:27:38 -0700

    Added more elements.

--- Kuali Sambal/lib/sambal/kuali_student.rb
+++ Kuali Sambal/lib/sambal/kuali_student.rb
@@ -6,8 +6,9 @@ module KualiStudent
 
     def self.header_elements
       element(:select_an_area) { |b| b.div(:id=>"gwt-uid-10") }
+      element(:logout_link) { |b| b.link(:text=>"Logout") }
 
-      action(:logout) { |b| b.link(:text=>"Logout").click }
+      action(:logout) { |p| p.logout_link.click }
     end
 
   end
