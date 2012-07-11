72043458e3887219d56307197b72c8ff2db2a07b
Abraham Heward <aheward@rsmart.com>
2012-07-11 07:36:54 -0700

    New elements for Kuali pages

--- Kuali Sambal/lib/sambal/kspages/home.rb
+++ Kuali Sambal/lib/sambal/kspages/home.rb
@@ -22,42 +22,6 @@ class KualiStudent::HomePage < KualiStudent::BasePage
 
 end
 
-class KualiStudent::WorkflowPreferences < KualiStudent::BasePage
-
-  header_elements
-  footer_elements
-
-  crucial_element(:frame_el) { |b| b.frame(:class=>"gwt-Frame KS-Action-List") }
-  element(:refresh_rate) { |b| b.frame_el.text_field(:name=>"preferences.refreshRate") }
-  element(:page_size) { |p| p.frame_el.text_field(:name=>"preferences.pageSize") }
-  element(:email_notification) { |b| b.frame_el.select(:name=>"preferences.emailNotification") }
-  element(:receive_primary_delegate_emails) { |b| b.frame_el.checkbox(:name=>"preferences.notifyPrimaryDelegation") }
-  element(:receive_secondary_delegate_emails) { |b| b.frame_el.checkbox(:name=>"preferences.notifySecondaryDelegation") }
-  element(:delegator_filter) { |b| b.frame_el.select(:name=>"preferences.delegatorFilter") }
-  element(:primary_delegate_filter) { |b| b.frame_el.select(:name=>"preferences.primaryDelegateFilter") }
-  element(:show_document_type) { |b| b.frame_el.checkbox(:name=>"preferences.showDocType") }
-  element(:show_title) { |b| b.frame_el.checkbox(:name=>"preferences.showDocTitle") }
-  element(:show_actionrequested) { |b| b.frame_el.checkbox(:name=>"preferences.showActionRequested") }
-  element(:show_initiator) { |b| b.frame_el.checkbox(:name=>"preferences.showInitiator") }
-  element(:show_delegator) { |b| b.frame_el.checkbox(:name=>"preferences.showDelegator") }
-  element(:show_date_created) { |b| b.frame_el.checkbox(:name=>"preferences.showDateCreated") }
-  element(:show_date_approved) { |b| b.frame_el.checkbox(:name=>"preferences.showDateApproved") }
-  element(:show_current_route_nodes) { |b| b.frame_el.checkbox(:name=>"preferences.showCurrentNode") }
-  element(:show_workgroup_request) { |b| b.frame_el.checkbox(:name=>"preferences.showWorkgroupRequest") }
-  element(:show_doc_route_status) { |b| b.frame_el.checkbox(:name=>"preferences.showDocumentStatus") }
-  element(:show_app_doc_status) { |b| b.frame_el.checkbox(:name=>"preferences.showAppDocStatus") }
-  element(:show_clear_fyi) { |b| b.frame_el.checkbox(:name=>"preferences.showClearFyi") }
-  element(:show_use_outbox) { |b| b.frame_el.checkbox(:name=>"preferences.useOutbox") }
-  element(:save_el) { |b| b.frame_el.button(:name=>"methodToCall.save") }
-  element(:reset_el) { |b| b.frame_el.link(:href=>"javascript:document.forms[0].reset()") }
-  element(:cancel_el) { |b| b.frame_el.link(:href=>"ActionList.do") }
-
-  action(:save) { |p| p.save_el.click }
-  action(:reset) { |p| p.reset_el.click }
-  action(:cancel) { |p| p.cancel_el.click }
-
-end
-
 class KualiStudent::ActionListFilter < KualiStudent::BasePage
 
   header_elements
--- Kuali Sambal/lib/sambal/pages/main_menu.rb
+++ Kuali Sambal/lib/sambal/pages/main_menu.rb
@@ -6,5 +6,62 @@ class MainMenu < BasePage
   wrapper_elements
 
   action(:enrollment) { |b| b.link(title: "Enrollment (in progress)").click }
+  action(:kuali_student_home) { |b| b.link(text: "Kuali Student Home").click }
+  action(:curriculum_management) { |b| b.link(text: "Curriculum Management").click }
+  action(:organization_management) { |b| b.link(text: "Organization Management").click }
+  action(:perform_rollover) { |b| b.link(text: "Perform Rollover").click }
+  action(:rollover_details) { |b| b.link(text: "Rollover Details").click }
+  action(:manage_course_offerings) { |b| b.link(text: "Manage Course Offerings").click }
+  action(:delete_target_term) { |b| b.link(text: "Delete Target Term").click }
+  action(:release_to_departments) { |b| b.link(text: "Release to Departments").click }
+  action(:activity_offering_lookup) { |b| b.link(text: "Activity Offering Lookup").click }
+  action(:activity_offering_lookup2) { |b| b.link(text: "Activity Offering Lookup 2").click }
+  action(:course_offering_info_lookup) { |b| b.link(title: "CourseOfferingInfo Lookup").click }
+  action(:enrollment_fee_info_lookup) { |b| b.link(title: "Enrollment Fee Info Lookup and Inquiry").click }
+  action(:format_offering_info_lookup) { |b| b.link(title: "Format Offering Info Lookup").click }
+  action(:socrolloverresultinfo_lookup) { |b| b.link(title: "SocRolloverResultInfo Lookup").click }
+  action(:socrolloverresultiteminfo_lookup) { |b| b.link(title: "SocRolloverResultItemInfo Lookup").click }
+  action(:term_info_lookup) { |b| b.link(title: "Term Info Lookup").click }
+  action(:course_info_lookup) { |b| b.link(title: "Course Info Lookup").click }
+  action(:new_course_offering) { |b| b.link(title: "Course Offering (New)").click }
+  action(:edit_course_offering) { |b| b.link(title: "Course Offering (Edit)").click }
+  action(:new_enrollment_fee_info) { |b| b.link(title: "Enrollment Fee Info (New)").click }
+  action(:new_format_offering_info) { |b| b.link(title: "Format Offering Info (New)").click }
+  action(:manage_registration_windows) { |b| b.link(title: "Manage Registration Windows And Appointments").click }
+  action(:registration_windows_lookup) { |b| b.link(title: "Registration Windows Lookup and Inquiry ").click }
+  action(:calendar_search)  { |b| b.link(title: "Calendar Search").click }
+  action(:create_holiday_calendar) { |b| b.link(title: "Create Holiday Calendar").click }
+  action(:holiday_calendar_lookup) { |b| b.link(title: "Holiday Calendar Lookup").click }
+  action(:holiday_calendar_search) { |b| b.link(title: "Holiday Calendar Search (dev link)").click }
+  action(:create_academic_calendar) { |b| b.link(title: "Create Academic Calendar").click }
+  action(:copy_academic_calendar) { |b| b.link(title: "Copy Academic Calendar").click }
+  action(:atp) { |b| b.link(title: "ATP").click }
+  action(:date_range) { |b| b.link(title: "Date Range").click }
+  action(:milestone) { |b| b.link(title: "Milestone").click }
+  action(:enumeration) { |b| b.link(title: "Enumeration").click }
+  action(:enumerated_value) { |b| b.link(title: "Enumerated Value").click }
+  action(:message) { |b| b.link(title: "Message").click }
+  action(:atp_type) { |b| b.link(title: "ATP Type").click }
+  action(:atp_duration_type) { |b| b.link(title: "ATP Duration Type").click }
+  action(:atp_seasonal_type) { |b| b.link(title: "ATP Seasonal Type").click }
+  action(:date_range_type) { |b| b.link(title: "Date Range Type").click }
+  action(:milestone_type) { |b| b.link(title: "Milestone Type").click }
+  action(:user_preferences) { |b| b.link(title: "User Preferences").click }
+  action(:quicklinks) { |b| b.link(title: "Quicklinks").click }
+  action(:routing_report) { |b| b.link(title: "Routing Report").click }
+  action(:routing_rules) { |b| b.link(title: "Routing Rules").click }
+  action(:routing_rules_delegation) { |b| b.link(title: "Routing Rules Delegation").click }
+  action(:routing_and_id_mgmt_doc_heirarchy) { |b| b.link(title: "Routing and Identity Management Document Type Hierarchy").click }
+  action(:edoc_lite) { |b| b.link(title: "eDoc Lite").click }
+  action(:people_flow) { |b| b.link(title: "People Flow").click }
+  action(:notification_search) { |b| b.link(title: "Notification Search").click }
+  action(:channel_subscriptions) { |b| b.link(title: "Channel Subscriptions").click }
+  action(:delivery_types) { |b| b.link(title: "Delivery Types").click }
+  action(:create_new_agenda) { |b| b.link(title: "Create New Agenda").click }
+  action(:agenda_lookup) { |b| b.link(title: "Agenda Lookup").click }
+  action(:attribute_definition_lookup) { |b| b.link(title: "Attribute Definition Lookup").click }
+  action(:term_lookup) { |b| b.link(title: "Term Lookup").click }
+  action(:term_specification_lookup) { |b| b.link(title: "Term Specification Lookup").click }
+  action(:category_lookup) { |b| b.link(title: "Category Lookup").click }
 
 end
